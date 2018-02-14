cf14b280046317603c886546bc3b84f0c3c0d8bb
Sam Naser <sammnaser@gmail.com>
2018-02-14 17:29:05 -0600

    Define Request endpoints, make work with database

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 
-var mongoose = require('mongoose');
-
 //so the server can handle POST requests
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
@@ -22,5 +20,63 @@ router.get('/', function(req, res){
 	res.json({message: "API Base Endpoint."});
 });
 
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test');
+
+var User = require('./models/user');
+var Request = require('./models/request');
+
+//-=-=-=-=-=-=-=-=-=-
+// ROUTE DEFINITIONS 
+//-=-=-=-=-=-=-=-=-=-
+
+router.route('/requests')
+
+    //create an article
+    .post(function(req, res){
+
+		console.log("POST: requests")
+
+        var request = new Request();
+        request.destination = req.body.destination;
+        request.desiredTime = req.body.desiredTime;
+		request.requester = req.body.requester;
+		request.timeBuffer = req.body.timeBuffer;
+
+		console.log(req.body.destination);
+		console.log(req.body.desiredTime);
+		console.log(req.body.requester); 
+		console.log(req.body.timeBuffer); 
+
+        //save auction
+        request.save(function(err){
+            //return the error in response if it exists
+            if (err){
+                res.send(err);
+                console.log(err);
+            }
+
+            res.json({message: 'Request created!'});
+        });
+
+    })
+
+    //get route
+    .get(function(req, res){
+		console.log("GET: requests")
+        Request.find(function(err, requests){
+            if (err){
+                res.send(err);
+                conosle.log(err);
+            }
+
+			res.json(requests);
+
+        });
+
+    });
+
 app.use('/api', router);
 app.listen(PORT);
+
+console.log('Application listening on PORT: ' + PORT);
